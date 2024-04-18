03d8e2e06cd5164dcc76cf5af2f3cb78e9ea5dd8
sebastianMustatoiu <ioan-sebastian.mustatoiu@s.unibuc.ro>
2024-04-18 20:37:06 +0300

    Afisare carti 1

--- Biblioteca/resources/js/Pages/Books/Books.jsx
+++ Biblioteca/resources/js/Pages/Books/Books.jsx
@@ -1,9 +1,22 @@
+import React from 'react';
 import { Link, Head } from '@inertiajs/react';
 import '/resources/css/Books/books.css';
-export default function Books( {books} ) {
+
+export default function Books({ books }) {
     return (
-        <>
-            <h1> Pagina cu carti </h1>
-        </>
+        <div className="books-container">
+            <h1 className="page-title"> Biblioteca: </h1>
+            <div className="books-grid">
+                {books.map((book) => (
+                    <div key={book.id} className="book-card">
+                        <img src={book.coverUrl} />
+                        <div className="book-details">
+                            <h2 className="book-title">{book.title}</h2>
+                            <p className="book-author">{book.author}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 }
