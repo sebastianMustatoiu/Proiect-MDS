72d42710ec65a82dc68d0ff04bec565eb15f70fb
Alexandru-Istrate <alexistrate2001@gmail.com>
2024-04-19 00:26:29 +0300

    Revert "Revert "Top Navigation Bar""
    This reverts commit c3fe50eec17f34f742427327d00cd104f217a4ac.

---
+++ Biblioteca/resources/js/Layouts/OurAuthenticatedLayout.jsx
@@ -0,0 +1,31 @@
+import styles from '/resources/css/Layouts/OurAuthenticatedLayout.module.css';
+import {Link} from "@inertiajs/react";
+import { ArrowDownOutline } from 'react-ionicons'
+
+
+export default function OurAuthenticatedLayout( {user, children} ) {
+    return (
+        <>
+         <nav className={styles.menu}>
+                 <div className={styles.leftButtons}>
+                 <Link href={route('books.index')}> Home </Link>
+                 </div>
+                     <div className={styles.dropDown}>
+                         {user.name}
+                         <ArrowDownOutline
+                             color={'#00000'}
+                             title={''}
+                             height="21px"
+                             width="21px"
+                             className={styles.icon}
+                         />
+                        <div className={styles.dropDownContent}>
+                            <Link href={route('profile.edit')}> Profile </Link>
+                            <Link method="post" href={route('logout')}> Log out </Link>
+                        </div>
+                    </div>
+         </nav>
+            {children}
+        </>
+    )
+}
