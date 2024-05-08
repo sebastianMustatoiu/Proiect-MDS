2ec53ba87544d5aeba494e86533e569fe1b0317e
Alexandru-Istrate <alexistrate2001@gmail.com>
2024-05-08 16:42:10 +0300

    Update vite.config.js

--- Biblioteca/vite.config.js
+++ Biblioteca/vite.config.js
@@ -3,6 +3,16 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    /*
+    server: {
+        host: '192.168.1.142', // replace with the IP address of the Homestead machine
+        https: false,
+        cors: true,
+        hmr: {
+            host: '192.168.1.142', // replace with the IP address of the Homestead machine
+        }
+    },
+    */
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
