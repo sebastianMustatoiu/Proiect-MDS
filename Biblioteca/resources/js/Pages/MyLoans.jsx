118e9bfb470fd485b80f146fedf7c46209f52c10
sebastianMustatoiu <ioan-sebastian.mustatoiu@s.unibuc.ro>
2024-06-12 01:29:55 +0300

    my loans page appearance

--- Biblioteca/resources/js/Pages/MyLoans.jsx
+++ Biblioteca/resources/js/Pages/MyLoans.jsx
@@ -1,32 +1,40 @@
 import Layout from "@/Layouts/Layout.jsx";
-import {Link} from "@inertiajs/react";
+import { Link } from "@inertiajs/react";
+import styles from "/resources/css/Layouts/myLoans.module.css";
 
 export default function MyLoans({ auth, loans }) {
     const content = (
         <>
-            <table>
-                <thead>
-                <tr>
-                    <th>Book</th>
-                    <th>Loan Date</th>
-                    <th>Return Date</th>
-                </tr>
-                </thead>
-                <tbody>
-                {loans.map((loan) => (
-                    <tr key={loan.id}>
-                        <td><Link href={route('books.show', { id: loan.book.id })}>{loan.book.title}</Link></td>
-                        <td>{loan.loan_date}</td>
-                        <td>{loan.return_date}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
+            <div className={styles.pageTitle}>My Loans</div>
+            <div className={styles.loanTableContainer}>
+                <table className={styles.loanTable}>
+                    <thead>
+                        <tr>
+                            <th>Book</th>
+                            <th>Loan Date</th>
+                            <th>Return Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {loans.map((loan) => (
+                            <tr key={loan.id}>
+                                <td>
+                                    <Link href={route('books.show', { id: loan.book.id })}>
+                                        {loan.book.title}
+                                    </Link>
+                                </td>
+                                <td>{loan.loan_date}</td>
+                                <td>{loan.return_date}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </>
     );
 
     return (
-        <Layout activeMenu={'None'} user={auth.user}>
+        <Layout activeMenu={"None"} user={auth.user}>
             {content}
         </Layout>
     );
